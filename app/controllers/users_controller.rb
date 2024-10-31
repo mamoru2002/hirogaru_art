e2bfb8680fd510a912bba73965eb131fe9d9780c
樫内守 <kashiuchi@kashiuchimamorunoMacBook-Air-2.local>
2024-10-31 16:05:43 +0900

    UsersControllerのcreateアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user  # プロフィールページなどにリダイレクト
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
